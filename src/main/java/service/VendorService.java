d4bceac662728c2c816c7e1b7d470e720a483104
ds <ds@ds601-04>
2018-04-16 16:47:11 +0900

    0416header

--- src/main/java/service/VendorService.java
+++ src/main/java/service/VendorService.java
@@ -85,34 +85,7 @@ public class VendorService {
 	
 	public List<VendorStatus> vendorStatusList(ListCommand listCommand) {
 		
-		Date from = listCommand.getFrom();
-		Date to = listCommand.getTo();
-		SimpleDateFormat dt = 
-				new SimpleDateFormat("yyyy-MM-dd hh:mm:ss"); 
-		Calendar cal = Calendar.getInstance();
-		int year, month, day;
-		String from_s="", to_s="";
-		
-		Date fromWord = (from == null)?new Date():from;
-		cal.setTime(fromWord);
-		year = cal.get(cal.YEAR);
-		month = cal.get(cal.MONTH) + 1 ;
-		day= (from == null)?1:cal.get(cal.DATE);
-		from_s = year+"-"+month+"-"+day+" 00:00:00.0";
-
-		Date toWord = (to == null)?new Date():to;
-		cal.setTime(toWord);
-		year = cal.get (cal.YEAR);
-		month = cal.get (cal.MONTH) + 1 ;
-		day= cal.get(cal.DATE);
-		to_s = year+"-"+month+"-"+day+" 23:59:59.0";
-		
-		try { 
-			from = dt.parse(from_s);
-			to = dt.parse(to_s);
-		} catch (ParseException e) { } 
-		listCommand.setFrom(from);
-		listCommand.setTo(to);
+	
 		
 		List<VendorStatus> results = sqlSession.selectList("vendorSQL.getStatusList");
 	
